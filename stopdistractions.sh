015768802d2b743093f5106d132f8698d8f4fcc2
Gustavo de Oliveira <gust.oliveira.ferreira@gmail.com>
2023-03-05 18:42:10 -0300

    Add instagram and youtube, change sed destiny to /etc/hosts

--- stopdistractions.sh
+++ stopdistractions.sh
@@ -10,22 +10,25 @@ social_media=(
 	facebook.com
 	www.facebook.com
 	api.facebook.com
+	www.youtube.com
+	youtube.com
+	api.youtube.com
+	instagram.com
+	api.instagram.com
+	www.instagram.com
 )
 
 if [[ $1 == "block" ]]; then
 	for i in "${social_media[@]}"; do
-		sed -i  "" "/^127.0.0.1 $i/d" /private/etc/hosts
-		echo "127.0.0.1 $i" >>  /private/etc/hosts
+		sed -i "/^127.0.0.1 $i/d" /etc/hosts
+		echo "127.0.0.1 $i" >>  /etc/hosts
 	done
 	
-	killall Firefox
-        pkill "Google Chrome"
-        pkill "Safari"
+	killall firefox
 fi
 
 if [[ $1 == "unblock" ]]; then
         for i in "${social_media[@]}"; do
-                sed -i "" "/^127.0.0.1 $i/d" /private/etc/hosts
+                sed -i "/^127.0.0.1 $i/d" /etc/hosts
         done
 fi
-
